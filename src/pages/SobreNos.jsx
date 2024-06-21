0c944a059c9af65c320870207ab62ad9e38fea77
R0b14 <rtth141200@gmail.com>
2024-06-20 23:01:45 -0300

    updates

--- src/pages/SobreNos.jsx
+++ src/pages/SobreNos.jsx
@@ -2,15 +2,16 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 const SobreNos = () => {
-    return (
-        <>
-            <NavBar />
-            <div className="div_teste">
-                <h2>Página Sobre Nós</h2>
-            </div>
-            <Footer />
-        </>
-    );
-}
+  return (
+    <>
+      <NavBar />
+      <div className="div_teste">
+        <h2>Página Sobre Nós</h2>
+        <button className="btn btn-outline-danger">Enviar</button>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default SobreNos
+export default SobreNos;
