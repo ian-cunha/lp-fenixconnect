363299b3eb9e3db8fbd85f6c01bd6c0cc11d1df8
r0b14-hp <56169778+r0b14@users.noreply.github.com>
2024-06-25 18:01:57 -0300

    sobre nos

--- src/pages/SobreNos.jsx
+++ src/pages/SobreNos.jsx
@@ -13,24 +13,30 @@ const SobreNos = () => {
       <main class="div_teste">
 
         <div className="container marketing">
-          <h1>Equipe</h1>
+          <h1 className="fw-bold">Equipe</h1>
 
           <div className="row">
-            <div className="col-lg-4">
+            <div className="col-lg-3">
               <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
-              <h2 className="fw-normal">Ian Cunha</h2>
+              <h2 className="fw-normal">Anthony Silva</h2>
               <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
               <p><a className="btn btn-secondary" href="#">Sobre &raquo;</a></p>
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg-3">
               <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
-              <h2 className="fw-normal">Robson Thiago</h2>
+              <h2 className="fw-normal">Cicero Alves</h2>
               <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
               <p><a className="btn btn-secondary" href="#">Sobre &raquo;</a></p>
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg-3">
               <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
-              <h2 className="fw-normal">Anthony Silva</h2>
+              <h2 className="fw-normal">Ian Cunha</h2>
+              <p>And lastly this, the third column of representative placeholder content.</p>
+              <p><a className="btn btn-secondary" href="#">Sobre &raquo;</a></p>
+            </div>
+            <div className="col-lg-3">
+              <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
+              <h2 className="fw-normal">Robson Thiago</h2>
               <p>And lastly this, the third column of representative placeholder content.</p>
               <p><a className="btn btn-secondary" href="#">Sobre &raquo;</a></p>
             </div>
